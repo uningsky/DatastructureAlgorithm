f679f379a995f75bac974fa4ec97ed140aa8594e
uningsky <uningsky@gmail.com>
2021-01-07 23:01:01 +0900

    A*알고리즘 슬라이딩 퍼즐

---
+++ A스타알고리즘/Board.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace A스타알고리즘
+{
+    public class Board : IEquatable<Board>
+    {
+        private int[,] _tiles;
+        private int _dimension;
+        private List<Board> _nodes = null;  
+        
+        public Board(int[,] tiles, int dimension)
+        {
+            this._tiles = tiles;
+            this._dimension = dimension; 
+        }
+
+        public List<Board> Neighbors()
+        {
+            List<Board> board = new List<Board>(); 
+
+            int zeroTileXIndex = 0;
+            int zeroTileYIndex = 0;
+            for (int i = 0; i < _tiles.GetLength(0); i++)
+            {
+                for (int j = 0; j < _tiles.GetLength(1); j++)
+                {
+                    if (_tiles[i, j] == 0)
+                    {
+                        zeroTileXIndex = i;
+                        zeroTileYIndex = j;
+                        break;
+                    }
+                }
+            }
+
+            // 위
+            if (zeroTileYIndex > 0)
+            {
+                
+                var zeroMoveUpBoard = new Board((int[,])_tiles.Clone(), _dimension);
+                zeroMoveUpBoard.TileSwap(zeroTileXIndex, zeroTileYIndex, zeroTileXIndex, zeroTileYIndex - 1);
+                board.Add(zeroMoveUpBoard); 
+            }
+
+            // 오른족
+            if (zeroTileXIndex < _dimension - 1)
+            {
+                var zeroMoveRightBoard = new Board((int[,])_tiles.Clone(), _dimension);
+                zeroMoveRightBoard.TileSwap(zeroTileXIndex, zeroTileYIndex, zeroTileXIndex + 1, zeroTileYIndex);
+                board.Add(zeroMoveRightBoard);
+            }
+
+            // 왼쪽
+            if (zeroTileXIndex > 0)
+            {
+                var zeroMoveLeftBoard = new Board((int[,])_tiles.Clone(), _dimension);
+                zeroMoveLeftBoard.TileSwap(zeroTileXIndex, zeroTileYIndex, zeroTileXIndex - 1, zeroTileYIndex);
+                board.Add(zeroMoveLeftBoard);
+            }
+
+            // 아래
+            if (zeroTileYIndex < _dimension - 1)
+            {
+                var zeroMoveDownBoard = new Board((int[,])_tiles.Clone(), _dimension);
+                zeroMoveDownBoard.TileSwap(zeroTileXIndex, zeroTileYIndex, zeroTileXIndex, zeroTileYIndex + 1);
+                board.Add(zeroMoveDownBoard);
+            }
+
+            return board; 
+        }
+
+        private void TileSwap(int sourceX, int sourceY, int targetX, int targetY)
+        {
+            int temp = _tiles[sourceX, sourceY];
+            _tiles[sourceX, sourceY] = _tiles[targetX, targetY];
+            _tiles[targetX, targetY] = temp;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj as Board);
+        }
+
+        public bool Equals(Board other)
+        {
+            if (Object.ReferenceEquals(this, other))
+            {
+                return true; 
+            }
+
+            if (_tiles.GetLength(0) != other._tiles.GetLength(0) || _tiles.GetLength(1) != other._tiles.GetLength(1))
+            { 
+                return false; 
+            }
+
+            for (int i = 0; i < _tiles.GetLength(0); i++)
+            {
+                for (int j = 0; j < _tiles.GetLength(1); j++)
+                {
+                    if (_tiles[i, j] != other._tiles[i, j])
+                    {
+                        return false;
+                    }
+                }
+                
+            }
+
+            return true; 
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < _tiles.GetLength(0); i++)
+            {
+                for (int j = 0; j < _tiles.GetLength(1); j++)
+                {
+                    builder.Append(_tiles[i, j]);
+                    builder.Append(" ");
+                }
+                builder.Append("\n");
+            }
+
+            return builder.ToString(); 
+        }
+    }
+}
---
+++ A스타알고리즘/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace A스타알고리즘
+{
+    class Program
+    {
+        // slider puzzle - 8 - 퍼즐
+        static void Main(string[] args)
+        {
+            // 0 : 빈칸
+            int[] goalBoard = { 1, 2, 3, 4, 5, 6, 7, 8, 0 };
+            int[] board = Suffle(3);
+
+            // 
+
+
+
+        }
+
+        private static void TileSwap(int[] array, int source, int target)
+        {
+            int temp = array[source];
+            array[source] = array[target];
+            array[target] = temp; 
+        }
+
+        private static int[] Suffle(int col)
+        {
+            int[] number = new int[col * col];
+            int[] puzzlePanel = new int[col * col];
+
+            for (int i = 0; i < number.Length; i++)
+            {
+                number[i] = i;
+            }
+
+            var numberList = number.ToList();
+
+            Random random = new Random();
+
+            for (int i = 0; i < puzzlePanel.Length; i++)
+            {
+                int randomIndex = random.Next(0, numberList.Count - 1);
+
+                puzzlePanel[i] = numberList[randomIndex];
+
+                numberList.RemoveAt(randomIndex);
+            }
+
+            StringBuilder builder = new StringBuilder(); 
+            for (int i = 0; i < puzzlePanel.Length; i++)
+            {
+                builder.Append(puzzlePanel[i]);
+                builder.Append(" ");
+
+                if ((i + 1) % col == 0)
+                {
+                    Console.WriteLine("{0}", builder);
+                    builder.Clear(); 
+                }
+            }
+
+            return puzzlePanel; 
+        }
+    }
+}
