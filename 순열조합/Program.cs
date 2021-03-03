39a0a751cbb59755c94bd0207bf0014e61ff24c4
uningsky <uningsky@gmail.com>
2021-03-03 15:14:10 +0900

    수열조합

--- 순열조합/Program.cs
+++ 순열조합/Program.cs
@@ -15,8 +15,9 @@ namespace 순열조합
 
             //CallMyCombi();
             //CallMyCombination(); 
-            CallMyPermutation();
-
+            //CallMyPermutation();
+            //CallCombinationForLoop();
+            CallPermutationForLoop(); 
         }
 
         static void CallAllPermutation()
@@ -130,6 +131,120 @@ namespace 순열조합
             }
 
         }
+
+        static void CallCombinationForLoop()
+        {
+            string[] source = { "apple", "banana", "orange", "lemon", "pine apple" };
+            int n = 5;
+            int k = 3;
+
+            // 0 1 2 3 4
+
+            int[] data = new int[k];
+
+            for (int i = 0; i < k; i++)
+            {
+                data[i] = i; 
+            }
+
+            string[] result = new string[k];
+            for (int l = 0; l < data.Length; l++)
+            {
+                result[l] = source[data[l]];
+            }
+            Console.WriteLine("{0}", string.Join(", ", result));
+
+            while (data[0] != n - k)
+            {
+                int index;
+                for (index = k - 1; index > 0; index--)
+                {
+                    if (data[index] != n - k + index)
+                    {
+                        break;
+                    }
+                }
+
+                data[index]++;
+
+                for (int j = index; j < k - 1; j++)
+                {
+                    data[j + 1] = data[j] + 1;
+                }
+
+                for (int l = 0; l < data.Length; l++)
+                {
+                    result[l] = source[data[l]];
+                }
+                Console.WriteLine("{0}", string.Join(", ", result));
+            }
+
+        }
+
+        static void CallPermutationForLoop()
+        {
+            string[] source = { "0", "1", "2", "3" };
+            int n = source.Length;
+
+            int[] data = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                data[i] = i; 
+            }
+
+            string[] result = new string[n];
+            for (int i = 0; i < n; i++)
+            {
+                result[i] = source[data[i]];
+            }
+
+            Console.WriteLine("{0}", string.Join(", ", result));
+
+            while (true)
+            {
+                int left = n - 2; 
+                
+                while (left > 0 && data[left] > data[left + 1])
+                {
+                    left--; 
+                }
+
+                if (left == 0 && data[left] > data[left + 1])
+                {
+                    break;
+                }
+
+                int right = n - 1;
+
+                while (data[left] > data[right])
+                {
+                    right--;
+                }
+
+                int temp = data[left];
+                data[left] = data[right];
+                data[right] = temp;
+
+                int start = left + 1;
+                int end = n - 1; 
+
+                while (start < end)
+                {
+                    temp = data[start];
+                    data[start++] = data[end];
+                    data[end--] = temp;
+                }
+
+                for (int i = 0; i < n; i++)
+                {
+                    result[i] = source[data[i]];
+                }
+
+                Console.WriteLine("{0}", string.Join(", ", result)); 
+            }
+        }
+
     }
 
 }
